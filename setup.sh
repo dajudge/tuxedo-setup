54774e1dd250b5971d0a1cecb45765d491d92930
Alex Stockinger <mail@alexstockinger.de>
2019-06-11 20:50:49 +0200

    Install docker

--- setup.sh
+++ setup.sh
@@ -79,3 +79,13 @@ if [[ $CMD = "all" ]] || [[ $CMD = "keyboard" ]]; then
 	modprobe tuxedo_keyboard
 	echo tuxedo_keyboard >> /etc/modules
 fi
+
+# Docker
+if [[ $CMD = "all" ]] || [[ $CMD = "docker" ]]; then
+	apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	apt-get update
+	apt-get install -y docker-ce docker-ce-cli containerd.io
+	usermod -a -G docker $SUDO_USER
+fi
